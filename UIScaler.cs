becb05ef51a18b8e0444a311b31d330492893d45
ArchiveTAG <91967453+ArchiveTAG@users.noreply.github.com>
2021-10-17 00:57:16 +0300

    UI Scaler for apps

---
+++ UIScaler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScaler : MonoBehaviour
+{
+    public float resoX, resoY;
+    public GameObject[] panels;
+    private CanvasScaler can;
+    // Start is called before the first frame update
+    void Start()
+    {
+        can = GetComponent<CanvasScaler>();
+        SetScale();
+    }
+
+    void SetScale()
+    {
+        resoX = (float)Screen.currentResolution.width;
+        resoY = (float)Screen.currentResolution.height;
+    
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].GetComponent<RectTransform>().sizeDelta = new Vector2(resoX,resoY);
+        }
+        
+    }
+}
